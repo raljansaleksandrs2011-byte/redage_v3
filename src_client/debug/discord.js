fc4b20fb6c713a299eb5dde623db719dad7d7239
raljansaleksandrs2011-byte <raljansaleksandrs2011@gmail.com>
2025-12-22 17:55:30 +0200

    Update discord.js

--- src_client/debug/discord.js
+++ src_client/debug/discord.js
@@ -1,14 +1,14 @@
 gm.discord = (detailedStatus) => {
-    let state = "на RedAge";
+    let state = "On Harbo Roleplay";
 
     if (global.localplayer && typeof global.localplayer.remoteId !== "undefined")
-        state = translateText('на RedAge под ID {0}', global.localplayer.remoteId);
+        state = translateText('On Harbo Roleplay under ID {0}', global.localplayer.remoteId);
 
     mp.discord.update(detailedStatus, state);
 }
 
 global.discordDefault = () => {
-    gm.discord(translateText('Наслаждается жизнью'))
+    gm.discord(translateText('Enjoying life'))
 };
 
 discordDefault ();
