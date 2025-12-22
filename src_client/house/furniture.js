fbee6c1f11fde0842dc0e423c193a92193974ae0
raljansaleksandrs2011-byte <raljansaleksandrs2011@gmail.com>
2025-12-22 18:12:24 +0200

    Update furniture.js

--- src_client/house/furniture.js
+++ src_client/house/furniture.js
@@ -1,8 +1,8 @@
-﻿gm.events.add('startEditing', (model) => {
+gm.events.add('startEditing', (model) => {
 	try
 	{
 		global.binderFunctions.GameMenuClose ();
-		gm.discord(translateText("Расставляет мебель в доме"));
+		gm.discord(translateText("Arranges furniture in the house"));
 		global.OnObjectEditor (mp.game.joaat (model), null, (pos, rot, _) => {				
 			mp.events.callRemote('acceptEdit', pos.x, pos.y, pos.z, rot.x, rot.y, rot.z);
 		},
