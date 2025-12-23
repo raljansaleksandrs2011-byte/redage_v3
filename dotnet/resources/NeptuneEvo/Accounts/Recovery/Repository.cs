fd2b627c0f8a58f7881398f63198a263e630de6c
raljansaleksandrs2011-byte <raljansaleksandrs2011@gmail.com>
2025-12-23 19:21:56 +0200

    Update Repository.cs

--- dotnet/resources/NeptuneEvo/Accounts/Recovery/Repository.cs
+++ dotnet/resources/NeptuneEvo/Accounts/Recovery/Repository.cs
@@ -1,4 +1,4 @@
-﻿using Database;
+using Database;
 using GTANetworkAPI;
 using NeptuneEvo.Handles;
 using LinqToDB;
@@ -54,7 +54,7 @@ namespace NeptuneEvo.Accounts.Recovery
                     return;
                 }
                 Utils.Analytics.HelperThread.AddUrl($"recovery?email={auntificationData.Email}&name={login}&code={sessionData.RecoveryCode}&ip={sessionData.Address}");
-                Notify.Send(player, NotifyType.Success, NotifyPosition.BottomCenter, $"Сообщение с кодом для восстановления пароля успешно выслано на {Generate.ObfuscateEmail(auntificationData.Email)}.", 5000);
+                Notify.Send(player, NotifyType.Success, NotifyPosition.BottomCenter, $"A message with a password recovery code has been successfully sent to {Generate.ObfuscateEmail(auntificationData.Email)}.", 5000);
                 Trigger.ClientEvent(player, "restorepassstep", 1);
             }
             catch (Exception e)
@@ -78,7 +78,7 @@ namespace NeptuneEvo.Accounts.Recovery
                 }
                 if (code == sessionData.RecoveryCode)
                 {
-                    Log.Debug($"{sessionData.RealSocialClub} удачно восстановил пароль!", nLog.Type.Info);
+                    Log.Debug($"{sessionData.RealSocialClub} successfully recovered the password!", nLog.Type.Info);
                     sessionData.RecoveryCode = null;
                     string newPassword = Generate.RandomString(9);
                     if (newPassword == null)
@@ -101,8 +101,8 @@ namespace NeptuneEvo.Accounts.Recovery
 
                     Autorization.Repository.AutorizationAccount(player, auntificationData.Login, auntificationData.Password).Wait();
                     
-                    Notify.Send(player, NotifyType.Success, NotifyPosition.BottomCenter, "Вы успешно восстановили доступ к аккаунту. Новый пароль выслан на емайл!", 5000);
-                    Notify.Send(player, NotifyType.Success, NotifyPosition.BottomCenter, "Помимо этого Вы можете изменить его прямо сейчас из игры введя /password и новый желаемый пароль через пробел. Пример [/password 123] без скобок.", 10000);
+                    Notify.Send(player, NotifyType.Success, NotifyPosition.BottomCenter, "You have successfully restored access to your account. A new password has been sent to your email.!", 5000);
+                    Notify.Send(player, NotifyType.Success, NotifyPosition.BottomCenter, "You can also change it right now from within the game by entering /password followed by a space and your new desired password. Example: [/password 123] without the brackets.", 10000);
                 }
                 else
                 {
